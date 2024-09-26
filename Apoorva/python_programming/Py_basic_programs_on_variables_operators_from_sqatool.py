3d2457da27b5b96d5491803fd923460aee28a48d
gitapoorva <apoorvashetty3@gmail.com>
2024-09-26 18:14:57 +0530

    python basic programs_ SQAtool

---
+++ Apoorva/python_programming/Py_basic_programs_on_variables_operators_from_sqatool.py
@@ -0,0 +1,123 @@
+#1.  Python Program to add two integer values.
+a= 10
+b = 20
+print(a+b)
+
+#2. Python Program to subtract two integer values.
+a = 20
+b = 10
+print(a-b)
+print(b-a)
+
+#3. Python program to multiply two numbers.
+
+a = 15
+b = 3
+c = 2
+print(a*b)
+print(a*b*c)
+
+#4. Python program to repeat a given string 5 times.
+
+string1 = "SQATools"
+print(string1*5)
+
+#5. Python program to get the Average of given numbers.
+
+a = 5
+b = 10
+c = 3
+average = (a+b+c)/3
+print("average of two numbers is:" , average)
+
+#6. Python program to get the median of given numbers.
+# Note: all the numbers should be arranged in ascending order
+ # Formula : (n+1)/2
+
+
+#7. Python program to print the square and cube of a given number.
+
+num1 = 7
+square = num1**2
+cube = num1**3
+print("square and cube value accordingly is:" , square, cube)
+
+
+#8. Python program to interchange values between variables.
+
+a = 10
+b = 20
+
+a, b  = b ,a
+print(a, b)
+
+#9 . Python program to solve this Pythagorous theorem.
+# Theorem : (a2 + b2 = c2)
+
+a = 3
+b = 4
+c = 5
+
+LHS = a**2 + b**2
+RHS = c**2
+print("LHS and RHS accordingly is :" , LHS, RHS)
+
+
+#10. Python program to solve the given math formula.
+
+# formula is : (a + b)2 = a^2 + b^2 + 2ab
+
+#11). Python program to solve the given math formula.
+# Formula : (a – b)2 = a^2 + b^2 – 2ab
+
+
+
+#12. Python program to solve the given math formula.
+# Formula : a2 – b2 = (a-b)(a+b)
+
+
+#13. Python program to solve the given math formula.
+# Formula : (a + b)3 = a3 + 3ab(a+b) + b3
+
+#14). Python program to solve the given math formula.
+ # Formula : (a – b)3 = a3 – 3a2b + 3ab2 – b3
+
+ #15 .  Python program to calculate the area of the square.
+# Formula : area = a*a
+
+#16. Python program to calculate the area of a circle.
+# Formula = PI*r*r
+
+#17. Python program to calculate the area of a cube.
+# Formula = 6*a*a
+
+#18). Python program to calculate the area of the cylinder.
+# Formula = 2*PI*r*h + 2*PI*r*r
+
+#19). Python program to check whether the given number is an Armstrong number or not.  # uses if else statements.
+# Example: 153 = 1*1*1 + 5*5*5 + 3*3*3
+
+#20. Python program to calculate simple interest.
+#Formula = P+(P/r)*t
+#P = Principle Amount
+#r = Anual interest rate
+# t = time
+
+#21. Python program to print the current date in the given format
+# Output: 2023 Jan 05
+# Note: Use the DateTime library
+
+#22.  Python program to calculate days between 2 dates.
+# Input date : (2023, 1, 5) (2023, 1, 22)
+# Output: 17 days
+
+
+# 27).  Python program to calculate compound interest.
+
+# 28). Python program to check the prime number.
+
+ # 39). Python program to calculate the volume of a sphere.
+# Formula = (4/3*pi*r^2)
+# r = radius
+# pi = 3
+
