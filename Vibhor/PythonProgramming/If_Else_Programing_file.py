316044652cc384d3d98576bb82ade1ed44dd96ef
Vibhor2203 <vibhorkhare23@gmail.com>
2024-09-27 22:22:00 +0530

    File update with given Programs

---
+++ Vibhor/PythonProgramming/If_Else_Programing_file.py
@@ -0,0 +1,17 @@
+marks= int(input("Enter the Marks Obtain by Student: "))
+if marks <40:
+    print("Fail")
+elif marks >=40 and marks <50:
+    print("GradecC")
+elif marks >=50 and marks <60:
+    print("Grade B")
+elif marks >=60 and marks <70:
+    print("Grade A")
+elif marks >=70 and marks <80:
+    print("Grade A+")
+elif marks >=80 and marks <90:
+    print("Grade A++")
+elif marks >=90 and marks <100:
+    print("Excellent")
+elif marks >100:
+    print("Invalid")
