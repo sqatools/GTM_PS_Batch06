842cd4e68885af6370da7c4e6972232618898487
Bi12Python <xovek11875@bitvoo.com>
2024-10-02 22:35:15 +0530

    add if condition programs

--- Deepesh/PythonProgramming/Python_If_else/Python_If_Else_condition.py
+++ Deepesh/PythonProgramming/Python_If_else/Python_If_Else_condition.py
@@ -5,7 +5,6 @@ else:
    code
 """
 
-
 a = 30
 b = 30
 
@@ -15,8 +14,6 @@ if a == b:
     print("a and b has equal values")
 else:
     print("a and b has different values")
-
-
 
 """
 # input always take user input in string format, we have to convert it
@@ -58,7 +55,6 @@ else:
     print("Given number can not divide by 2 and 3 :", num_a)
 """
 
-
 """
 or condition
 
@@ -68,6 +64,7 @@ True or True : True
 False or False : False
 """
 
+"""
 # write a program to check the given is divisible by 2 or 3
 print("_"*50)
 num_a = int(input("Please enter the values :"))
@@ -76,3 +73,110 @@ if num_a%2 == 0 or num_a%3 == 0:
     print("The number can divide by 2 or 3 :", num_a)
 else:
     print("The number can not divide by both 2 or 3 :", num_a)
+"""
+
+"""
+# If - elif - else condition
+
+if cond1:
+    code
+elif cond2:
+     code
+elif cond3:
+     code
+else:
+    code 
+"""
+
+"""
+# logical operators
+> : greater than
+< : less than
+>= : greater equal to
+<= : less equal to
+!= : not equal
+== : equal to
+in : in operator
+is : is operator
+"""
+# --------------------------------------------
+"""
+print("_"*50)
+# write a python program to check which number have greater value among three
+# variables
+
+p = 90
+q = 80
+r = 90
+
+if p > q and p > r:
+    print("P has greater value :", p)
+elif q > p and q > r:
+    print("Q has greater value :", q)
+elif r > p and r > q:
+    print("R has greater value :", r)
+elif p == q:
+    print("P and Q has same value :")
+elif p == r:
+    print("P and R has same value :")
+elif q == r:
+    print("R and Q has same value :")
+else:
+    print("No one has greater value")
+
+"""
+# --------------------------------------------
+
+"""
+# write a python program to implement the exam result marking
+marks = int(input("Please enter the marks:"))
+if marks > 100:
+    print("Please enter valid marks, can not more than 100")
+elif 40 < marks <= 50:
+    print("Passed with D grade")
+elif 50 < marks <= 60:
+    print("Passed with C grade")
+elif 60 < marks <= 70:
+    print("Passed with B grade")
+elif 70 < marks <= 80:
+    print("Passed with A grade")
+elif 80 < marks <= 90:
+    print("Passed with A+ grade")
+elif 90 < marks <= 100:
+    print("Passed with Excellent A++ grade")
+else:
+    print("Failed in examination")
+
+"""
+
+#################### Nested If condition #############
+
+"""
+if cond1:
+     code
+     if cond2:
+         code
+         if cond3:
+             code
+         else:
+             code
+     else:
+         code
+else:
+     code
+"""
+
+# write a python program to check given number can divide by 2 and 3 with
+# nested if
+
+num_b = 14
+if num_b % 2 == 0:
+    print("The number can divide 2:", num_b)
+    if num_b % 3 == 0:
+        print("The number can divide 3:", num_b)
+    else:
+        print("The number can not divide 3:", num_b)
+else:
+    print("The number can not divide 2:", num_b)
+
+# Q1. write a python program to show interview process with nested if condition
