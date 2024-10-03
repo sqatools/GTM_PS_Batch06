fd2c08f12a99cb0db190ba329ff6dd8b1a10f1bb
GitPushpa-Raj <pushpakalaraj2005@gmail.com>
2024-10-03 11:52:31 -0400

    Program for elif and nested if else

--- Pushpa/PythonProgramming/if_else_condition_practice.py
+++ Pushpa/PythonProgramming/if_else_condition_practice.py
@@ -1,25 +1,27 @@
 # Interview process demonstration using nested if conditions
 """
 # with elif
-tech_score = int(input("Enter technical interview score (out of 100): "))
-hr_score = int(input("Enter HR interview score (out of 100): "))
+rusume_round = int(input("Resume score: "))
+tech_score = int(input("Enter technical interview score: "))
+hr_score = int(input("Enter HR interview score: "))
 
-# Step 1: Technical Interview
-if tech_score < 70:
-    print("Technical Interview: Failed")
-    print("Sorry, you did not pass the technical interview.")
+# Step 1: Resume selection
+if rusume_round >= 80:
+    print("Resume selection: Passed")
+
+elif tech_score > 70:
+        print("Technical Interview: Pass")
+        print("Sorry, you did not pass the technical interview.")
 
 # Step 2: HR Interview
-elif hr_score < 60:
-    print("Technical Interview: Passed")
-    print("HR Interview: Failed")
-    print("Sorry, you did not pass the HR interview.")
-
+elif hr_score >= 80:
+        print("HR Interview: Failed")
+        print("Congratulations! You have been selected for the job.")
 # Final Success Case
 else:
-    print("Technical Interview: Passed")
-    print("HR Interview: Passed")
-    print("Congratulations! You have been selected for the job.")
+    print("Resume selection: Failed")
+    print("Technical Interview: Failed")
+    print("HR Interview: Failed")
 """
 
 
@@ -28,21 +30,20 @@ rusume_round = int(input("Resume score: "))
 tech_score = int(input("Enter technical interview score: "))
 hr_score = int(input("Enter HR interview score: "))
 
-# Step 1: Technical Interview
+# Interview
 if rusume_round >= 90:  # Passed technical interview
     print("Resume is eligible: Passed")
 
-    # Technical score interview
-    if tech_score >= 60:  # Passed HR interview
+    # Nested if for Technical Round
+    if tech_score >= 70:
         print("Technical interview: Passed")
-        print("Congratulations! You have been passed .")
     else:
-        print("HR Interview: Failed")
-        print("Sorry, you did not pass the HR interview.")
+        print("Technical Interview: Failed")
+        print("Sorry, you did not pass the Technical interview.")
 
 
     # Nested if for HR interview
-    if hr_score >= 60:  # Passed HR interview
+    if hr_score >= 90:
         print("HR Interview: Passed")
         print("Congratulations! You have been selected for the job.")
     else:
