9175edf10a1b7484602b9cbc2828b9b1854b6e7d
gitapoorva <apoorvashetty3@gmail.com>
2024-10-04 17:07:51 +0530

    python if-else programs_ SQAtool

--- Apoorva/python_programming/if_else_programs.py
+++ Apoorva/python_programming/if_else_programs.py
@@ -1,11 +1,12 @@
 #1. Python program to check given number is divided by 3 or not.
-
+'''
 num1 = float(input("enter the number:"))
 
 if num1%3 == 0:
     print("number is divisible by 3:", num1)
 else:
     print("number is not divisible by 3:")
+'''
 
 #3.  If else program to assign grades as per total marks.
 #marks > 40: Fail
@@ -17,10 +18,11 @@ else:
 #marks 90 – 100: grade Excellent
 #marks > 100: Invalid marks
 
+'''
 total_marks = float(input("enter the total marks scored:"))
 
-if   total_marks <40:
-    print("Fail")
+if  total_marks >100:
+    print("Invalid marks: Please enter valid marks")
 elif  40<= total_marks < 50:
     print("the grade is grade C")
 elif 50 <= total_marks < 60:
@@ -34,4 +36,78 @@ elif 80 <= total_marks <90:
 elif 90 <= total_marks < 100:
     print("the grade is grade Excellent!")
 else:
-    print("Invalid marks: Please enter valid marks")
+    print("Failed in examination")
+    '''
+#############################
+print("444444444444444444444444444444444")
+#4. Python program to check the given number divided by 3 and 5
+'''
+num2 = float(input("Enter the number to check if divisible by 3 and 5: "))
+
+if num2%3 == 0 and num2 % 5 == 0:
+    print("The number is divisible by 3 and 5", num2)
+
+else:
+    print("The number isn't divisible by 3 and 5")
+    
+'''
+#####################################################################
+
+# 5. Python program to print the square of the number if it is divided by 11.
+
+'''num1 = float(input("Enter the number::"))
+
+if num1%11 == 0:
+    print("the number is didvisible by 11 and the square of the number is:", num1**2)
+else:
+    print("the number is not divisible by 11")
+    
+'''
+
+##################################################################
+
+#7). Python program to check given number is odd or even.
+'''
+num1 = float(input("Enter the number::"))
+
+if num1%2 == 0:
+    print("the given number is even")
+else:
+    print("the number is odd")
+'''
+
+##################################################################
+
+#8. Python program to check a given number is part of the Fibonacci series from 1 to 10.
+
+# 0,1,1,2,3,5,8 # only upto 10
+
+'''fib = [0,1,1,2,3,5,8]
+num1 = float(input("Enter the number::"))
+
+if num1 in fib:
+    print("the number is part of fibonacci series")
+else:
+    print("the number is not in fibonacci series")
+'''
+
+############################################################
+
+# 9.  Python program to check authentication with the given username and password.
+''''
+username1 = "Apoorva"
+password1 = "apoorva123"
+
+username = input("enter valid Username:")
+password = input("enter valid password:")
+
+if username == username1 and password ==password1:
+    print("Logged in successfully")
+else:
+    print("please enter valid username/password")
+''''
+
+##########################################################
+
+# 10). Python program to validate user_id in the list of user_ids.
+
