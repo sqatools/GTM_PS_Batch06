4e5906bb39b5e26c68901de47c32a79a0f7b0b78
gitsushma123 <yvsushma3@gmail.com>
2024-10-09 12:28:13 +0530

    pattern programs practice

--- Sushma/python_programming/if_practice/practice program.py
+++ Sushma/python_programming/if_practice/practice program.py
@@ -97,6 +97,14 @@ for i in range(0,6,1):
         print("*",end=" ")
     print()
 
+"""
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+
 for i in range(5,0,-1):
     for j in range(i):
         print("*",end=" ")
