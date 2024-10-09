63140844b6da62089b9dd2dee125191df69d55b0
gitsushma123 <yvsushma3@gmail.com>
2024-10-09 14:12:05 +0530

    pattern programs practice

--- Sushma/python_programming/if_practice/practice program.py
+++ Sushma/python_programming/if_practice/practice program.py
@@ -84,19 +84,22 @@ fact=1
 for i in range(num,0,-1):
     fact =fact*i
     print("factorial num:",i,fact)"""
+
+#Q) 1.print pattern
 """
-Q)print pattern
 *
 * *
 * * * 
 * * * *
 * * * * *
 """
-for i in range(0,6,1):
+
+"""for i in range(0,6,1):
     for j in range(i):
         print("*",end=" ")
-    print()
+    print()"""
 
+#Q) 2.print pattern
 """
 * * * * *
 * * * *
@@ -105,7 +108,27 @@ for i in range(0,6,1):
 *
 """
 
-for i in range(5,0,-1):
+"""for i in range(5,0,-1):
     for j in range(i):
         print("*",end=" ")
-    print()
+    print()"""
+
+#Q)swap numbers
+"""a=10
+b=20
+print("before swapping a:",a)
+print("before swapping b:",b)
+a,b = b,a
+print("after swapping a:",a)
+print("after swapping b:",b)"""
+
+#Q) fibonacci  number
+num = int(input("Enter the number of terms for the Fibonacci sequence: "))
+
+i, j = 0, 1  # Initialize the first two Fibonacci numbers
+
+for _ in range(num):  # Loop num times
+    print(i, end=" ")  # Print the current Fibonacci number
+    k = i + j   # Calculate the next Fibonacci number
+    i = j         # Update i to the current j
+    j = k       # Update j to the next Fibonacci number
