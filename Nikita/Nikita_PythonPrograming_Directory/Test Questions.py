d0bafd0a2c945e5e65de0dc1e2073c4e47840cd4
gitnikita123 <nikitamuleypimr1921@gmail.com>
2024-10-13 11:44:00 +0530

    Answers of assignment

---
+++ Nikita/Nikita_PythonPrograming_Directory/Test Questions.py
@@ -0,0 +1,41 @@
+"""1. Python program to check whether the input number is a square of 6 or not.
+Input = 37
+
+2. Python program to print the square of the number if it is divided by 3 and cube if divisible by 7
+
+3. Write a python program to print factorial of number if it is divisible by 3.
+
+4. write a python print prime numbers between 1 to 50.
+
+5. Write a python program to print the average of 5 numbers from 10-15.
+
+6. Write a python program get score of even nunber and cube odd number from given list.
+list1 = [ 10, 13, 6, 7, 17, 3, 8, 19]
+
+7. write a python repeated the string 5 time if length of string is even and repeat string 7 times if length is odd.
+
+input_str = take input from user.
+
+
+8). Write a python program  to print below pattern of numbers
+
+2
+4 6
+8 10 12
+14 16 18 20
+
+
+9).  Write a python program  to print below pattern of numbers
+
+0
+0 1
+0 1 2
+0 1 2 3
+0 1 2
+0 1
+0
+
+10). Write a python to check given number is palindrome or not, ( don't use data type conversation, use while loop reverse the number.
+
+n1 : 121 , palindrome : True
+n1 : 123 , palindrome : False"""
