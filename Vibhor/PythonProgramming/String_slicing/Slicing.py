a726cffd46ee4f499d437e04bc2c6e0d1a25f7e6
Vibhor2203 <vibhorkhare23@gmail.com>
2024-10-14 22:55:09 +0530

    String slice test

---
+++ Vibhor/PythonProgramming/String_slicing/Slicing.py
@@ -0,0 +1,14 @@
+# Virat is best Indian Batsman
+str3 = "Indian is Batsman Virat best"
+first = str3[-10:-5:1]
+print("first: ", first)
+second = str3[7:9]
+print("second: ", second)
+third = str3[-4:]
+print("third: ", third)
+fourth = str3[0:7:1]
+print("fourth: ", fourth)
+fifth = str3[10:17:1]
+print("fifth: ", fifth)
+Result= f"{first} {second} {third} {fourth} {fifth}"
+print("Result= ",Result)
