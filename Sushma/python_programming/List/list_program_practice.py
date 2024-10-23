91c6f5ffbdabdd6f063398ad2f82838e076a5c9a
gitsushma123 <yvsushma3@gmail.com>
2024-10-23 22:30:25 +0530

    list

--- Sushma/python_programming/List/list_program_practice.py
+++ Sushma/python_programming/List/list_program_practice.py
@@ -4,13 +4,13 @@ for val in list2:
     if sq >25 :
         print(sq)"""
 
-"""list2=[4,7,9,12,5,2,10,3,6]
+list2=[4,7,9,12,5,2,10,3,6]
 for val in list2:
     #print(val)
     if val % 3 == 0  and val % 4 ==0 :
         print(val**2)
     elif val %5==0:
-        print(val**3)"""
+        print(val**3)
 
 list3=[4,7,8,100,9,12,45]
 max_val = 0
