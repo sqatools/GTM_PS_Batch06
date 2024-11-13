200901c9ae679acb049eaba539307aa5a208521d
Madhuri-420 <vemakotimadhuri@gmail.com>
2024-11-13 18:03:40 +0100

    python_file_Handling

--- Madhuri/Python_programming/Python_File_Handling.py
+++ Madhuri/Python_programming/Python_File_Handling.py
@@ -11,37 +11,35 @@ def read_file(filepath):
     print(data)
     file.close()
 # read_file from current location
-# read_file("read_data.txt")
+read_file("read_data.txt")
 
 # read file from specific path
-#read_file("C:\Test")
+read_file("C:\\Test\\News.txt")
 
 ############ open file in write mode###############
 def write_to_file(filepath,content):
     file = open(filepath,"w")
-    data = file.write()
+    file.write(content)
     # file.write(content)
     file.close()
 
 
 
 # 1.file doest not exist: it will create a new with specified name and add content  to the file
-# write_to_file("write_data.txt", "India lost second T20 match with South Africa")
+write_to_file("write_data.txt", "India lost second T20 match with South Africa")
 
-# 2. Existing file: write method will over write the existing content of the file
+# 2. Existing file: write method will overwrite the existing content of the file
 write_to_file("write_old_file.txt","India lost second t20 match with south Africa")
-
-
 ##########Append  content to the file############
 print("-"* 50)
 def Append_content_to_file(filepath,content):
 # open file in append mode
-  file  = open (filepath,"a")
+  file  = open(filepath,"a")
   file.write(content)
   file.close()
 
 
-#1.file doesnot exist:Append mode also create anew file ,if file doesnot exist.
+# 1.file does not exist:Append mode also create anew file ,if file does not exist.
 Append_content_to_file("append_data.txt", "Hell0 world")
-#2. Existing file:Append mode update new content to existing file at end of file
+# 2. Existing file:Append mode update new content to existing file at end of file
 Append_content_to_file("append_existing_data.txt"," \nToday we are celebrating Diwali in india")
