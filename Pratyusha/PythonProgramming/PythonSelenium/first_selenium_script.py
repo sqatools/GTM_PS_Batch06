748e9cc2bf0dc58bb86a7fcc2356d6fd821f6f64
Pratyusha0826 <npratyusha26@gmail.com>
2024-11-29 22:13:18 +0530

    selenium first script

--- Pratyusha/PythonProgramming/PythonSelenium/first_selenium_script.py
+++ Pratyusha/PythonProgramming/PythonSelenium/first_selenium_script.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 
 
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.implicitly_wait(10)
@@ -10,3 +11,4 @@ driver.implicitly_wait(10)
 driver.get("https://www.google.co.in")
 time.sleep(5)
 
+driver.close()
