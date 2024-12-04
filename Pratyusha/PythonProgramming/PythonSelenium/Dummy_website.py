b9f6e35a18e36ad82a4761889267330fafc5bd0c
Pratyusha0826 <npratyusha26@gmail.com>
2024-12-04 23:34:46 +0530

    dummy website practice with xpaths

--- Pratyusha/PythonProgramming/PythonSelenium/Dummy_website.py
+++ Pratyusha/PythonProgramming/PythonSelenium/Dummy_website.py
@@ -10,26 +10,73 @@ driver.maximize_window()
 driver.implicitly_wait(10)
 driver.get("https://automationbysqatools.blogspot.com/2021/05/dummy-website.html")
 
+website_name = driver.find_element(By.XPATH, "//h3[contains(text(), 'Dummy Website')]").text
+print(website_name)
+time.sleep(2)
+
 # TAG_NAME
 header = driver.find_element(By.TAG_NAME, "h1")
 print("header value: ", header.text)
+time.sleep(2)
+
+choose_option = driver.find_element(By.XPATH, "//*[@value='radio_123']")
+choose_option.click()
+time.sleep(2)
+
+first_name = driver.find_element(By.XPATH, "(//*[@name='firstname'])[1]")
+first_name.send_keys("Pratyusha")
+time.sleep(2)
+
+last_name = driver.find_element(By.XPATH, "(//input[@name='firstname'])[2]")
+last_name.send_keys("Nalam")
+time.sleep(2)
+
 # ID Locator
 dob_elem = driver.find_element(By.ID, "birthday")
 dob_elem.send_keys("08/26/2000")
-time.sleep(3)
+time.sleep(2)
+
+choose_gender = driver.find_element(By.XPATH, "//input[@id='female']")
+choose_gender.click()
+time.sleep(2)
+
+print("Choose gender is selected: ", choose_gender.is_selected())
+
 # Name Locator
-frm_city = driver.find_element(By.NAME, "fromcity")
+# frm_city = driver.find_element(By.NAME, "fromcity")
+frm_city = driver.find_element(By.XPATH, "//input[@name='fromcity']")
 frm_city.send_keys("Pune")
-time.sleep(3)
-# Link Text
+time.sleep(2)
+
+dest_city = driver.find_element(By.XPATH, "//input[@id='destcity']")
+dest_city.send_keys("Maharashtra")
+time.sleep(2)
+
+dept_date = driver.find_element(By.XPATH, "//input[@name='departdate']")
+dept_date.send_keys("12/04/2024")
+time.sleep(2)
+
+return_date = driver.find_element(By.XPATH, "//*[@name='returndate']")
+return_date.send_keys("12/04/2024")
+time.sleep(2)
+
+visa_date = driver.find_element(By.XPATH, "//input[contains(@name, 'visadate')]")
+visa_date.send_keys("12/04/2024")
+time.sleep(2)
+
+receive_platform = driver.find_element(By.XPATH, "//input[contains(@id, 'whatsapp')]")
+receive_platform.click()
+print("Ticket receiving platform is selected: ", receive_platform.is_selected())
+time.sleep(2)
+"""# Link Text
 python_selenium_link = driver.find_element(By.LINK_TEXT, "Python Selenium")
 python_selenium_link.click()
 time.sleep(3)
 # PARTIAL_LINK_TEXT
 pytest_framework = driver.find_element(By.PARTIAL_LINK_TEXT, "Pytest")
-pytest_framework.click()
+pytest_framework.click()"""
 
 
-time.sleep(5)
+# time.sleep(5)
 
 driver.close()
