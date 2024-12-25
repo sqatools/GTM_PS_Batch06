84833652dbb6846633123dea53aec0c4dd463b52
Bi12Python <xovek11875@bitvoo.com>
2024-12-25 23:23:05 +0530

    framework log file name update

--- Deepesh/PytestFramework/AutomationFramework/tests/conftest.py
+++ Deepesh/PytestFramework/AutomationFramework/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
-from selenium import  webdriver
+from datetime import datetime
+from selenium import webdriver
+
 
 @pytest.fixture(scope="class")
 def get_driver(request):
@@ -7,5 +9,12 @@ def get_driver(request):
     driver.maximize_window()
     driver.implicitly_wait(10)
     request.cls.driver = driver
-    yield # teardown
+    yield  # teardown
     driver.close()
+
+
+def pytest_configure(config):
+    """ Create a log file if log_file is not mentioned in *.ini file"""
+    if not config.option.log_file:
+        timestamp = datetime.now().strftime('%d-%m-%Y_%H_%M_%S')
+        config.option.log_file = "logs/" + timestamp + "_execution.log"
